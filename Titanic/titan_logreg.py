0cdc36fb54fc81ee670d9544cbd8b9bd1515cc3f
Igor Basov <mrbazzik@gmail.com>
2015-06-02 16:01:16 +0300

    Adds logreg titan

---
+++ Titanic/titan_logreg.py
@@ -0,0 +1,81 @@
+﻿import scipy as sp
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import precision_recall_fscore_support
+
+
+df = pd.read_csv("c:\\Users\\Basov_il\\Documents\\GitHub\\ML-tasks\\Titanic\\titanic_train.csv")
+df = df.loc[~pd.isnull(df.Embarked),:]
+df = df.loc[~pd.isnull(df.Age),:]
+
+x = df.loc[:,['Name','Pclass','Sex','Age','SibSp','Parch','Fare','Embarked']]
+y = df.loc[:,"Survived"]
+
+##x.Age = x.Age.fillna(x.Age.mean())
+x['SexN'] = [1]*len(x)
+x.loc[x.Sex=='male','SexN']=2
+x = x.drop("Sex",1)
+
+x['EmbarkedN'] = [1]*len(x)
+x.loc[x.Embarked=='Q','EmbarkedN']=2
+x.loc[x.Embarked=='S','EmbarkedN']=3
+x = x.drop("Embarked",1)
+
+x['Call']=[0]*len(x)
+x['Call']=[('Mr.' in x.loc[k,'Name'])+1 for k in x.index]
+x['Call']=[('Master.' in x.loc[k,'Name'])+2 for k in x.index]
+x['Call']=[('Mrs.' in x.loc[k,'Name'])+3 for k in x.index]
+x['Call']=[('Miss.' in x.loc[k,'Name'])+4 for k in x.index]
+
+
+x['Mrs']=[0]*len(x)
+x['Mrs']=[('Mrs.' in x.loc[k,'Name']) for k in x.index]
+
+x = x.drop("Name",1)
+
+x['ClassFare']=x['Pclass']*x['Fare']
+x['SexParch']=x['SexN']*x['Parch']
+x['SexClass']=x['SexN']*x['Pclass']
+x['SexCall']=x['SexN']*x['Call']
+##x['ClassCall']=x['Pclass']*x['Call']
+x['CallParch']=x['Call']*x['Parch']
+
+x['MrsParch']=x['Mrs']*x['Parch']
+x['MrsSibSp']=x['Mrs']*x['SibSp']
+
+x['SexCallClass']=x['SexN']*x['Call']*x['Pclass']
+
+##x['AgeClass']=x['Age']*x['Pclass']
+##x['SexSibSp']=x['SexN']*x['SibSp']
+##x['SexAge']=x['SexN']*x['Age']
+x = (x-sp.mean(x))/sp.std(x)
+
+
+n_train = 500
+x_train = x.iloc[:n_train,:]
+y_train = y.iloc[:n_train]
+x_test = x.iloc[n_train:,:]
+y_test = y.iloc[n_train:]
+
+##x_test = x_test[~pd.isnull(x_test.Age)]
+##y_test = y_test[~pd.isnull(x_test.Age)]
+
+sizes=[]
+e_train = []
+e_test = []
+for i in range(2,len(x_train)):
+    x_t = x_train.iloc[range(0,i),:]
+    y_t = y_train.iloc[range(0,i)]
+    clf = LogisticRegression(C=20)
+    clf.fit(x_t,y_t)
+    e_train.append(1-clf.score(x_t,y_t))
+    e_test.append(1-clf.score(x_test,y_test))
+    sizes.append(i)
+plt.plot(sizes, e_train)
+plt.plot(sizes, e_test)
+fscores = precision_recall_fscore_support(y_test, clf.predict(x_test), average='micro')
+print(fscores)
+for i in range(0,len(x.columns)):
+    print("%s - %s"%(x.columns[i], clf.coef_[0][i]))
+plt.show()
---
+++ Titanic/titan_nn2.py
@@ -0,0 +1,50 @@
+﻿import scipy as sp
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn import neighbors
+from sklearn.cross_validation import KFold
+
+
+df = pd.read_csv("c:\\Users\\Basov_il\\Documents\\GitHub\\ML-tasks\\Titanic\\titanic_train.csv")
+df = df.loc[~pd.isnull(df.Embarked),:]
+
+x = df.loc[:,['Pclass','Sex','Age','SibSp','Parch','Fare','Embarked']]
+y = df.loc[:,"Survived"]
+
+x.Age = x.Age.fillna(x.Age.mean())
+x['SexN'] = [0]*len(x)
+x.SexN = x.Sex=='male'
+x = x.drop("Sex",1)
+
+x['EmbarkedN'] = [1]*len(x)
+x.loc[x.Embarked=='Q','EmbarkedN']=2
+x.loc[x.Embarked=='S','EmbarkedN']=3
+x = x.drop("Embarked",1)
+
+x = (x-sp.mean(x))/sp.std(x)
+
+cv = KFold(n=len(x), n_folds=10, shuffle=True)
+scores=[]
+for train, test in cv:
+    print(train.shape)
+    print(test.shape)
+    x_test, y_test=x.iloc[test,:], y.iloc[test]
+    sizes=[]
+    errors_train=[]
+    errors_test=[]
+    for i in range(2, len(train)):
+        x_train, y_train = x.iloc[range(0,i),:], y.iloc[range(0,i)]
+        knn = neighbors.KNeighborsClassifier(n_neighbors=2, weights='distance')
+        knn.fit(x_train,y_train)
+        error_train = 1 - knn.score(x_train,y_train)
+        error_test = 1 - knn.score(x_test,y_test)
+        sizes.append(i+1)
+        errors_train.append(error_train)
+        errors_test.append(error_test)
+    plt.plot(sizes,errors_train)
+    plt.plot(sizes,errors_test)
+    plt.show()
+    scores.append(1-errors_test[len(errors_test)-1])
+print("Mean=%s, Std=%s"%(sp.mean(scores),sp.std(scores)))
+##plt.plot(range(0,len(scores)),scores)
+##plt.show()
