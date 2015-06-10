9f8dcbda1b046a6a35554812b7a112b780f27f41
Igor Basov <mrbazzik@gmail.com>
2015-06-10 18:53:43 +0300

    OK

--- Titanic/scripts/test.py
+++ Titanic/scripts/test.py
@@ -5,54 +5,118 @@ from sklearn.grid_search import GridSearchCV
 from sklearn.cross_validation import KFold
 import pickle as pkl
 import json as js
+from sklearn import preprocessing
 
 def prepareData(df):
-##    df = df.drop(['Name','Ticket','Cabin'], 1)
-    
-##  treating nans
-    age_mean = df.Age.mean()
-##    df.Age = df.Age.fillna(age_mean)
-    df.Age = df.Age.fillna(-1)
 
     mode_embarked = df.Embarked.mode()[0]
     df.Embarked = df.Embarked.fillna(mode_embarked)
 
-    fare_means = pd.pivot_table(df, values='Fare',index=['Pclass'], aggfunc='mean')
+    df['Gender'] = df.Sex.map({'male':1, 'female':0}).astype(float)
+
+    fare_means = pd.pivot_table(df, values='Fare',index=['Pclass'], aggfunc='median')
     df.Fare = df[['Fare','Pclass']].apply(lambda x: fare_means[x.Pclass] if pd.isnull(x.Fare) else x.Fare,1)
-    
-    df['Gender'] = df.Sex.map({'male':1, 'female':0}).astype(int)
-##    df['Port'] = df.Embarked.map({'C':1, 'S':2, 'Q':3}).astype(int)
-    dum = pd.get_dummies(df.Embarked, prefix='Embarked')
-    df = pd.concat([df,dum], 1)
 
     titles=['Mr.', 'Master.','Mrs.','Miss.','Dr.','Rev.','Major.','Col.','Capt.','Dona.','Lady.','Sir.','Don.','Countess.','Jonkheer.','Mme.','Ms.','Mlle.']
-    common=['Mr', 'Master','Mrs','Miss','Dr','Mme','Ms','Mlle']
-    arist = ['Dona','Lady','Sir','Don','Countess','Jonkheer','Rev']
-    military=['Major','Col','Capt']
+##    common=['Mr', 'Master','Mrs','Miss','Dr','Mme','Ms','Mlle','Rev','Jonkheer','Col']
+##    arist = ['Dona','Lady','Sir','Don','Countess']
+##    military=['Major','Capt']
+
+    mrlist = ['Mr','Don', 'Major', 'Capt', 'Jonkheer', 'Rev', 'Col','Sir','Dr']
+    mrslist = ['Countess', 'Mme','Mrs','Dona','Lady']
+    misslist = ['Mlle', 'Ms','Miss']
     
     def getTitle(x):
         for i in titles:
             if i in x:
                 return i[:len(i)-1]
 
-    df['Title'] = df.Name.map(lambda x: getTitle(x))
-    
-##    for i in titles:
-##        title = i[:len(i)-1]
-##        df['Title_'+title] = (df.Title==title).astype(int)
+    def getTitleGroup(x):
+        if x in mrlist:
+            return 'Mr'
+        else:
+            if x in mrslist:
+                return 'Mrs'
+            else:
+                if x in misslist:
+                    return 'Miss'
+                else:
+                    if x=='Master':
+                        return 'Master'
+    def replace_titles(x):
+        title=x['Title']
+        if title in ['Mr','Don', 'Major', 'Capt', 'Jonkheer', 'Rev', 'Col', 'Sir']:
+            return 'Mr'
+        elif title in ['Master']:
+            return 'Master'
+        elif title in ['Countess', 'Mme','Mrs','Dona','Lady']:
+            return 'Mrs'
+        elif title in ['Mlle', 'Ms','Miss']:
+            return 'Miss'
+        elif title =='Dr':
+            if x['Sex']=='Male':
+                return 'Mr'
+            else:
+                return 'Mrs'
+        elif title =='':
+            if x['Sex']=='Male':
+                return 'Master'
+            else:
+                return 'Miss'
+        else:
+            return title               
         
-    df['TitleGroup_Common']=df.Title.map(lambda x: 1 if x in common else 0)
-    df['TitleGroup_Arist']=df.Title.map(lambda x: 1 if x in arist else 0)
-    df['TitleGroup_Military']=df.Title.map(lambda x: 1 if x in military else 0)
-                
+    df['Title'] = df.Name.map(lambda x: getTitle(x))
+
+    df['Title'] = df.apply(lambda x:replace_titles(x),1)
+
+    le = preprocessing.LabelEncoder()
  
-##    dum = pd.get_dummies(df.Title, prefix='Title')
-##    df = pd.concat([df,dum],1)
-##    dum = pd.get_dummies(df.TitleGroup, prefix='TitleGroup')
-##    df = pd.concat([df,dum],1)
-   
+    dum = pd.get_dummies(df.Title, prefix='Title')
+    df = pd.concat([df,dum],1)
+
+    age_means = pd.pivot_table(df, values='Age', index=['Title'], aggfunc='mean')
+    df.Age = df[['Age','Title']].apply(lambda x: age_means[x.Title] if pd.isnull(x.Age) else x.Age,1)
     
-    df = df.drop(['Name','Ticket','Cabin','Sex','Embarked','Title'], 1)
+    df['AgeCat'] = df.Age.map(lambda x: 'child' if x<=10 else 'adult' if x<=30 else 'senior' if x<=60 else 'aged')
+
+    df['FamilySize'] = df.SibSp+df.Parch
+##    df['Family'] = df.SibSp*df.Parch
+    df['FarePerPerson']=df.Fare/(df.FamilySize+1)
+
+    df['PclassFare'] = df.Pclass*df.FarePerPerson
+    df['PclassAge'] = df.Pclass*df.Age
+
+##    df['HighLow']=df['Pclass']
+##    df.loc[ (df.FarePerPerson<8) ,'HighLow'] = 'Low'
+##    df.loc[ (df.FarePerPerson>=8) ,'HighLow'] = 'High'
+
+    le.fit(df['Sex'] )
+    x_sex=le.transform(df['Sex'])
+    df['Sex']=x_sex.astype(float)
+    
+    le.fit( df['Ticket'])
+    x_Ticket=le.transform( df['Ticket'])
+    df['Ticket']=x_Ticket.astype(float)
+
+    le.fit(df['Title'])
+    x_title=le.transform(df['Title'])
+    df['Title'] =x_title.astype(float)
+
+##    le.fit(df['HighLow'])
+##    x_hl=le.transform(df['HighLow'])
+##    df['HighLow']=x_hl.astype(float)
+
+
+    le.fit(df['AgeCat'])
+    x_age=le.transform(df['AgeCat'])
+    df['AgeCat'] =x_age.astype(float)
+
+##    le.fit(df['Embarked'])
+##    x_emb=le.transform(df['Embarked'])
+##    df['Embarked']=x_emb.astype(float)
+
+    df = df.drop(['Name','Cabin','Embarked'], 1)
     return df
 
 config = js.loads(open('../SETTINGS.json').read())
@@ -62,10 +126,10 @@ submit_path = "../"+config["SUBMISSION_PATH"]
 
 dft = pd.read_csv(test_path+"/test.csv")
 df = prepareData(dft)
-test_data = df.values
+test_data = df.drop('PassengerId',1)
 
 gs = pkl.load(open(model_path+"/model_rf.pkl",'rb'))
-output = gs.predict(test_data[:,1:])
+output = gs.predict(test_data)
 
-result = pd.DataFrame({'PassengerId': test_data[:,0].astype(int), 'Survived': output.astype(int)})
+result = pd.DataFrame({'PassengerId': df.PassengerId.astype(int), 'Survived': output.astype(int)})
 result.to_csv(submit_path+"/result_rf.csv", index=False)
--- Titanic/scripts/train.py
+++ Titanic/scripts/train.py
@@ -2,99 +2,159 @@ import pandas as pd
 import scipy as sp
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.grid_search import GridSearchCV
-from sklearn.cross_validation import KFold
+from sklearn.cross_validation import train_test_split,StratifiedShuffleSplit,StratifiedKFold, cross_val_score
 import json as js
 import pickle as pkl
 from sklearn.preprocessing import Imputer
 from sklearn.pipeline import Pipeline
+from scipy  import stats
+from sklearn import preprocessing
 
 def prepareData(df):
-##    df = df.drop(['Name','Ticket','Cabin'], 1)
-    
-##  treating nans
-    age_mean = df.Age.mean()
-##    df.Age = df.Age.fillna(age_mean)
-    df.Age = df.Age.fillna(-1)
 
     mode_embarked = df.Embarked.mode()[0]
     df.Embarked = df.Embarked.fillna(mode_embarked)
 
-    fare_means = pd.pivot_table(df, values='Fare',index=['Pclass'], aggfunc='mean')
+    df['Gender'] = df.Sex.map({'male':1, 'female':0}).astype(float)
+
+    fare_means = pd.pivot_table(df, values='Fare',index=['Pclass'], aggfunc='median')
     df.Fare = df[['Fare','Pclass']].apply(lambda x: fare_means[x.Pclass] if pd.isnull(x.Fare) else x.Fare,1)
-    
-    df['Gender'] = df.Sex.map({'male':1, 'female':0}).astype(int)
-##    df['Port'] = df.Embarked.map({'C':1, 'S':2, 'Q':3}).astype(int)
-    dum = pd.get_dummies(df.Embarked, prefix='Embarked')
-    df = pd.concat([df,dum], 1)
 
     titles=['Mr.', 'Master.','Mrs.','Miss.','Dr.','Rev.','Major.','Col.','Capt.','Dona.','Lady.','Sir.','Don.','Countess.','Jonkheer.','Mme.','Ms.','Mlle.']
-    common=['Mr', 'Master','Mrs','Miss','Dr','Mme','Ms','Mlle']
-    arist = ['Dona','Lady','Sir','Don','Countess','Jonkheer','Rev']
-    military=['Major','Col','Capt']
+##    common=['Mr', 'Master','Mrs','Miss','Dr','Mme','Ms','Mlle','Rev','Jonkheer','Col']
+##    arist = ['Dona','Lady','Sir','Don','Countess']
+##    military=['Major','Capt']
+
+    mrlist = ['Mr','Don', 'Major', 'Capt', 'Jonkheer', 'Rev', 'Col','Sir','Dr']
+    mrslist = ['Countess', 'Mme','Mrs','Dona','Lady']
+    misslist = ['Mlle', 'Ms','Miss']
     
     def getTitle(x):
         for i in titles:
             if i in x:
                 return i[:len(i)-1]
 
-    df['Title'] = df.Name.map(lambda x: getTitle(x))
-    
-##    for i in titles:
-##        title = i[:len(i)-1]
-##        df['Title_'+title] = (df.Title==title).astype(int)
+    def getTitleGroup(x):
+        if x in mrlist:
+            return 'Mr'
+        else:
+            if x in mrslist:
+                return 'Mrs'
+            else:
+                if x in misslist:
+                    return 'Miss'
+                else:
+                    if x=='Master':
+                        return 'Master'
+    def replace_titles(x):
+        title=x['Title']
+        if title in ['Mr','Don', 'Major', 'Capt', 'Jonkheer', 'Rev', 'Col', 'Sir']:
+            return 'Mr'
+        elif title in ['Master']:
+            return 'Master'
+        elif title in ['Countess', 'Mme','Mrs','Dona','Lady']:
+            return 'Mrs'
+        elif title in ['Mlle', 'Ms','Miss']:
+            return 'Miss'
+        elif title =='Dr':
+            if x['Sex']=='Male':
+                return 'Mr'
+            else:
+                return 'Mrs'
+        elif title =='':
+            if x['Sex']=='Male':
+                return 'Master'
+            else:
+                return 'Miss'
+        else:
+            return title               
         
-##    df['TitleGroup_Common']=df.Title.map(lambda x: 1 if x in common else 0)
-##    df['TitleGroup_Arist']=df.Title.map(lambda x: 1 if x in arist else 0)
-##    df['TitleGroup_Military']=df.Title.map(lambda x: 1 if x in military else 0)
-                
+    df['Title'] = df.Name.map(lambda x: getTitle(x))
+
+    df['Title'] = df.apply(lambda x:replace_titles(x),1)
+
+    le = preprocessing.LabelEncoder()
  
-##    dum = pd.get_dummies(df.Title, prefix='Title')
-##    df = pd.concat([df,dum],1)
-##    dum = pd.get_dummies(df.TitleGroup, prefix='TitleGroup')
-##    df = pd.concat([df,dum],1)
-   
+    dum = pd.get_dummies(df.Title, prefix='Title')
+    df = pd.concat([df,dum],1)
+
+    age_means = pd.pivot_table(df, values='Age', index=['Title'], aggfunc='mean')
+    df.Age = df[['Age','Title']].apply(lambda x: age_means[x.Title] if pd.isnull(x.Age) else x.Age,1)
     
-    df = df.drop(['Name','Ticket','Cabin','Sex','Embarked','Title'], 1)
-    cols = df.columns.tolist()
-    cols = cols[1:2]+cols[0:1]+cols[2:]
-    df = df[cols]
+    df['AgeCat'] = df.Age.map(lambda x: 'child' if x<=10 else 'adult' if x<=30 else 'senior' if x<=60 else 'aged')
+
+    df['FamilySize'] = df.SibSp+df.Parch
+    df['Family'] = df.SibSp*df.Parch
+    df['FarePerPerson']=df.Fare/(df.FamilySize+1)
+
+    df['PclassFare'] = df.Pclass*df.FarePerPerson
+    df['PclassAge'] = df.Pclass*df.Age
+
+    df['HighLow']=df['Pclass']
+    df.loc[ (df.FarePerPerson<8) ,'HighLow'] = 'Low'
+    df.loc[ (df.FarePerPerson>=8) ,'HighLow'] = 'High'
+
+    le.fit(df['Sex'] )
+    x_sex=le.transform(df['Sex'])
+    df['Sex']=x_sex.astype(float)
+    
+    le.fit( df['Ticket'])
+    x_Ticket=le.transform( df['Ticket'])
+    df['Ticket']=x_Ticket.astype(float)
+
+    le.fit(df['Title'])
+    x_title=le.transform(df['Title'])
+    df['Title'] =x_title.astype(float)
+
+    le.fit(df['HighLow'])
+    x_hl=le.transform(df['HighLow'])
+    df['HighLow']=x_hl.astype(float)
+
+
+    le.fit(df['AgeCat'])
+    x_age=le.transform(df['AgeCat'])
+    df['AgeCat'] =x_age.astype(float)
+
+    le.fit(df['Embarked'])
+    x_emb=le.transform(df['Embarked'])
+    df['Embarked']=x_emb.astype(float)
+
+    df = df.drop(['PassengerId','Name','Cabin'], 1)
     return df
 
 config = js.loads(open('../SETTINGS.json').read())
 train_path = "../"+config["TRAIN_DATA_PATH"]
 model_path = "../"+config["MODEL_PATH"]
+seed = int(config["SEED"])
 
 dft = pd.read_csv(train_path+"/train.csv")
 df = prepareData(dft)
 
-train_data = df.values
+x_train = df.drop("Survived",1)
+y_train = df.Survived
 
-imputer = Imputer(missing_values=-1)
+x_train, x_test, y_train, y_test = train_test_split(x_train, y_train, test_size=0.2, random_state=seed)
 
-model = RandomForestClassifier(n_estimators=100)
+model = RandomForestClassifier(n_estimators=300, random_state=seed)
 
-pipeline = Pipeline([('imp',imputer),('clf',model)])
-params = {'clf__max_features':[0.5,1], 'clf__max_depth':[5, None], 'imp__strategy': ['mean','median']}
+params = {'max_features':[0.5,1,'auto'], 'max_depth':[5, None]}
 
-gs = GridSearchCV(pipeline, params, cv=10)
-gs.fit(train_data[:,2:], train_data[:,0])
+gs = GridSearchCV(model, params, cv=StratifiedShuffleSplit(y_train, n_iter=10, test_size=0.2, random_state=seed))
+gs.fit(x_train, y_train)
+print("Best Grid search results:")
 print(gs.best_score_)
 print(gs.best_params_)
+c = [[df.columns[2+i], gs.best_estimator_.feature_importances_[i]] for i in range(0,len(df.columns)-2)]
+d = sorted(c, key=lambda x: x[1],reverse=True)
+for i in d:
+	print("%s - %s"%(i[0],i[1]))
 
-cur_scores = gs.grid_scores_[0].cv_validation_scores.tolist()
-print(cur_scores)
-stop
-df.Age = df.Age.map(lambda x: df.Age.median() if x==-1 else x)
-##print('Score = %s'%(gs.best_score_))
-train_data = df.values
-model = RandomForestClassifier(n_estimators=300,max_features=gs.best_params_['clf__max_features'],max_depth=gs.best_params_['clf__max_depth'])
-model = model.fit(train_data[:,2:], train_data[:,0])
+print("Train accuracy:")
+scores = cross_val_score(gs.best_estimator_, x_train, y_train, cv=3, scoring="accuracy")
+print(scores.mean(), scores)
 
-pkl.dump(model, open(model_path+"/model_rf.pkl",'wb'))
-##cv = KFold(n=len(train_data),n_folds=5)
-##for tr, test in cv:
-##    gs.fit(train_data[tr,2:],train_data[tr,0])
-##    y_p = gs.predict(train_data[test,2:])
-##    print('Score cv = %s'%(sp.mean(y_p==train_data[test,0])))
+print("Test accuracy:")
+scores = cross_val_score(gs.best_estimator_, x_test, y_test, cv=3, scoring="accuracy")
+print(scores.mean(), scores)
 
-
+pkl.dump(gs.best_estimator_, open(model_path+"/model_rf.pkl",'wb'))
